7dc402af53f315c99bcff7badbdabf3770534376
Erik J. Sandal <erik.sandal@bouvet.no>
2019-09-23 08:48:54 +0200

    new initial

--- site.js
+++ site.js
@@ -3,13 +3,13 @@
     // JSON-LD
     var structuredData = {
         schema: {
-            "@context": "http://schema.org",
+            "@context": "https://schema.org",
             "@type": "Person",
             "image": "/img/ejs.jpg",
             "jobTitle": "Software Engineer",
             "name": "Erik J. Sandal",
-            "birthDate": "1992.01.20",
-            "url": "https://sandal.cloud",
+            "alternateName": "Erik Johan Sandal",
+            "url": "https://sandal.cloud/",
             "sameAs": [
                 "https://no.linkedin.com/in/erikjsandal/", 
                 "https://github.com/Slippersen/"
@@ -18,7 +18,7 @@
         init: function () {
             var scriptEl = document.createElement('script');
             scriptEl.type = 'application/ld+json';
-            scriptEl.innerHTML = JSON.stringify(structuredData);
+            scriptEl.innerHTML = JSON.stringify(structuredData.schema);
             document.body.appendChild(scriptEl);
         }
     }
